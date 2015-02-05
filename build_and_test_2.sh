870bfbb84850536cf07be16e3d864b0c81652296
davidkristola <david95037@yahoo.com>
2015-02-05 01:51:49 -0800

    Build environment
    Updated the rebuild script (need to rename it since it remakes lex and
    parse code), and made a new build and test script.

---
+++ build_and_test_2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function mk_t() {
+gnatmake -I.. -g test_avm;
+}
+
+function mk_v() {
+gnatmake -I.. volea;
+}
+
+function mk_m() {
+gnatmake -I.. volem;
+}
+
+function tst_3() {
+volec ../test_3.vole && volem ../test_3.volec Driver go 1;
+}
+
+function tst_4() {
+volec ../test_4.vole && volem ../test_4.volec Driver go 1;
+}
+
+cd obj
+(mk_t && mk_v && mk_m && ../rebuild && tst_3 && tst_4 && cd .. && obj/test_avm) | tee results.txt
+
